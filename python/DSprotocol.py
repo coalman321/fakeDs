8f140f222885410138dbbf281f0adb74c4af6fa2
coalman321 <coalman321@users.noreply.github.com>
2018-09-03 16:06:06 -0400

    cleanups

--- python/DSprotocol.py
+++ python/DSprotocol.py
@@ -4,18 +4,21 @@ import threading
 import struct
 from enum import Enum
 
+
 class ControlMode(Enum):
     TELEOP = 0B00000000
-    TEST =   0B00000001
-    AUTO =   0B00000010
+    TEST = 0B00000001
+    AUTO = 0B00000010
+
 
 class AllianceStation(Enum):
-    RED1 =  0B00000000
-    RED2 =  0B00000001
-    RED3 =  0B00000010
+    RED1 = 0B00000000
+    RED2 = 0B00000001
+    RED3 = 0B00000010
     BLUE1 = 0B00000011
     BLUE2 = 0B00000100
     BLUE3 = 0B00000101
+
 
 class ToRioPacket:
 
@@ -37,53 +40,50 @@ class ToRioPacket:
         self.wantReset = reset
         self.allianceStation = station
 
-    def getPacket(self):
+    def getpacket(self):
         self.counter += 1
         control = 0B00000000
         control |= self.isEstop << 7
         control |= self.isFms << 3
         control |= self.isEnabled << 2
-        control |= self.controlmode.value & 0xff
+        control |= self.controlmode.value
         request = 0B00000000
         request |= self.wantRestart << 3
         request |= self.wantReset << 2
-        return struct.pack('BBBBBB', self.getCounterMSB() & 0xff, self.getCounterLSB() & 0xff, 0x01, control & 0xff,
-                           request & 0xff, self.allianceStation.value & 0xff)
+        return struct.pack('BBBBBB', self.getcountermsb(), self.getcounterlsb(), 0x01,
+                           control, request, self.allianceStation.value)
 
-    def getCounterMSB(self):
+    def getcountermsb(self):
         return self.counter >> 8
 
-    def getCounterLSB(self):
+    def getcounterlsb(self):
         return self.counter & 0xff
+
 
 class DSprotocol:
 
-    __teamnumber = 0
+    teamnumber = 0
     wantStop = False
-    __ip = ''
-    __toRioPacket = ToRioPacket
-    __packetThread = threading.Thread
+    ip = ''
+    toRioPacket = ToRioPacket
+    packetThread = threading.Thread
 
     def __init__(self, teamnum):
         self.teamnumber = teamnum
-        self.ip = self.getAddr()
+        self.ip = self.getaddr()
         self.toRioPacket = ToRioPacket(False, False, False, ControlMode.TELEOP, False, False, AllianceStation.RED1)
-        print(self.toRioPacket.getPacket())
-        self.packetThread = threading.Thread(target=self.packetHandler)
+        print(self.toRioPacket.getpacket())
+        self.packetThread = threading.Thread(target=self.packethandler)
         self.packetThread.start()
 
-
-    def setPacket(self, newPacket):
-        self.toRioPacket = newPacket
-
-    def getAddr(self):
+    def getaddr(self):
         try:
             return socket.gethostbyname("roborio-" + str(self.teamnumber) + "-frc.local")
         except socket.gaierror:
             print("failed to find host by name falling back to fixed IP")
             return socket.gethostbyname('10.' + str(self.teamnumber)[:2] + '.' + str(self.teamnumber)[2:] + '.3')
 
-    def packetHandler(self):
+    def packethandler(self):
         print("attempting connection to RIO")
         print(self.ip)
         output = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -92,7 +92,7 @@ class DSprotocol:
             output.bind(("", 1150))
             print("connected to RIO begining broadcast")
             while not self.wantStop:
-                output.sendto(self.toRioPacket.getPacket(), (self.ip, 1110))
+                output.sendto(self.toRioPacket.getpacket(), (self.ip, 1110))
                 time.sleep(0.019)
         except Exception as e:
             print(e)
